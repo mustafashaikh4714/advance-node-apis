c906cac0df2ca02999263e1c89a64f798fc85ce3
Mustafa Mohammad Shaikh <mustafas@imtac.com>
2021-07-25 17:11:56 +0530

    init

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import { App } from '@tsxp/core'
+import { json, urlencoded } from 'express'
+import { Home } from 'src/controllers/home'
+
+export const { app, listen } = new App({
+  prefix: '/api',
+  middlewares: [
+    json(),
+    urlencoded({ extended: true })],
+  controllers: [Home]
+})
+
+let n
