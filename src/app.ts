71b02a823dca668684ad393a153a55684c1f0c11
mustafashaikh4714 <shaikhmustafa.4714@gmail.com>
2021-07-25 18:00:35 +0530

    update app

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,8 @@ import { Home } from 'src/controllers/home'
 
 export const { app, listen } = new App({
   prefix: '/api',
-  middlewares: [json(), urlencoded({ extended: true })],
+  middlewares: [json(),
+  urlencoded({ extended: true })],
   controllers: [Home]
 })
 
-let n
