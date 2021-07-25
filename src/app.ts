52476e447c344e5b6422a632d890cf97cf3908e5
Mustafa Mohammad Shaikh <mustafas@imtac.com>
2021-07-25 17:49:23 +0530

    lint fix

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,6 @@ import { Home } from 'src/controllers/home'
 
 export const { app, listen } = new App({
   prefix: '/api',
-  middlewares: [
-    json(),
-    urlencoded({ extended: true })],
+  middlewares: [json(), urlencoded({ extended: true })],
   controllers: [Home]
 })
-
-let n
