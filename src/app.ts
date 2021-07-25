d91e338d8b3d2c06e31df655373aa1b332f9396b
mustafashaikh4714 <shaikhmustafa.4714@gmail.com>
2021-07-25 19:05:49 +0530

    update eslint

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,10 @@
-import { App } from '@tsxp/core'
-import { json, urlencoded } from 'express'
-import { Home } from 'src/controllers/home'
+import { App } from '@tsxp/core';
+import { json, urlencoded } from 'express';
+import { Home } from 'src/controllers/home';
 
 export const { app, listen } = new App({
   prefix: '/api',
-  middlewares: [
-    json(),
-    urlencoded({ extended: true })
-  ],
+  middlewares: [json(),
+  urlencoded({ extended: true })],
   controllers: [Home]
 })
-
-
-
-
-
-
-
